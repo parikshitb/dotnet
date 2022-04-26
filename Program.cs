8318d5c809731d6edfb67c8ade3fc7236e4b98ab
parikshit <parikshit@Kalyanis-MacBook-Pro.local>
2022-04-26 07:49:23 +0530

    initial checkin

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("This shall take a while . . . . . . .");
+var cts = new CancellationTokenSource();
+var token = cts.Token;
+//cts.CancelAfter(10000); //does not call async operation? 
+
+try
+{
+    MyClassAsync.LongWork(token);
+    cts.Cancel();
+    Console.WriteLine("All good comes to those who wait");
+}
+catch (TaskCanceledException)
+{
+    Console.WriteLine("Arrggghh!! Can't wait");
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Arrggghh!! Can't wait");
+}
+catch(AggregateException)
+{
+    Console.WriteLine("Multiple exceptions");
+}
+finally
+{
+    cts.Dispose();
+}
+
+public class MyClassAsync
+{
+    public static Task LongWork(CancellationToken ct)
+    {
+        Task? task = null;
+        ct.Register(CallMeIfCancelled);
+        task = Task.Run(() =>
+        {
+            Task.Delay(5000);
+            Console.WriteLine("Long work started");
+            Task.Delay(5000);
+        });
+        return task;
+    }
+
+    private static void CallMeIfCancelled()
+    {
+        Console.WriteLine("Task Cancelled :( ");
+        throw new TaskCanceledException();
+    }
+}
+
+
