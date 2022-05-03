ee5e986259dfedac004164d29bf98a61d11b8052
parikshit <parikshit@Kalyanis-MacBook-Pro.local>
2022-05-03 06:11:15 +0530

    code cleanup

--- Program.cs
+++ Program.cs
@@ -1,29 +1,34 @@
 ﻿Console.WriteLine("This shall take a while . . . . . . .");
 var cts = new CancellationTokenSource();
-//cts.CancelAfter(1000); 
 
 try
 {
-    var task = MyClass.LongWork(cts.Token);
+    /*
+     * For a time consuming work, wait for it to finish for x seconds
+     * Continue the execution if work is not finished within the time
+     */
+    var task = MyClass.TimeConsumingWork(cts.Token);
     Thread.Sleep(1000);
     if (!task.IsCompleted)
     {
-        Console.WriteLine("Initiate Cacnelling. . . . . . . . .");
+        Console.WriteLine("Initiate Cacnelling >>.");
         cts.Cancel();
     }
     Console.WriteLine("All good comes to those who wait");
 }
-catch (TaskCanceledException)
+//https://docs.microsoft.com/en-us/dotnet/standard/parallel-programming/exception-handling-task-parallel-library
+//https://docs.microsoft.com/en-us/archive/msdn-magazine/2009/brownfield/aggregating-exceptions#figure-3-aggregateexception-in-parallel-invocation
+catch (AggregateException aggex)
 {
-    Console.WriteLine("Arrggghh!! Can't wait");
-}
-catch (OperationCanceledException)
-{
-    Console.WriteLine("Arrggghh!! Can't wait");
-}
-catch(AggregateException)
-{
-    Console.WriteLine("Multiple exceptions");
+    aggex.Handle((ex) =>
+    {
+        if (ex is TaskCanceledException)
+        {
+            Console.WriteLine("Task Cancellation well received.");
+            return true;
+        }
+        return false; //Considered as unhandled exception
+    });
 }
 finally
 {
@@ -32,22 +37,24 @@ finally
 
 public class MyClass
 {
-    public static Task LongWork(CancellationToken token)
+    public static Task TimeConsumingWork(CancellationToken token)
     {
-        Task? task = null;
+        //https://docs.microsoft.com/en-us/dotnet/csharp/fundamentals/functional/discards
         _ = token.Register(CallMeIfCancelled);
+
+        Task? task = null;
         task = Task.Run(() =>
         {
-            Console.WriteLine("Long work started");
+            Console.WriteLine("Long work started.");
             Thread.Sleep(5000);
-            Console.WriteLine("Long work Ended");
+            Console.WriteLine("Long work Ended.");
         });
         return task;
     }
 
     private static void CallMeIfCancelled()
     {
-        Console.WriteLine("Task Cancelled. . . . . .");
+        Console.WriteLine("Task Cancelled.");
         throw new TaskCanceledException();
     }
 }
